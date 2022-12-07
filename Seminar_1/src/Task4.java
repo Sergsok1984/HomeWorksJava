85e32ccdc9be9bf37db2fc5b5ae62c7a549c3ff9
Sergey Sokolov <sergsok1984@gmail.com>
2022-12-07 23:45:41 +0300

    add change to Task4

--- Seminar_1/src/Task4.java
+++ Seminar_1/src/Task4.java
@@ -3,6 +3,7 @@
 //     Требуется восстановить выражение до верного равенства.
 //     Предложить хотя бы одно решение или сообщить, что его нет.
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task4 {
@@ -10,20 +11,27 @@ public class Task4 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите выражение вида q + w = e, неизвестные цифры заменить на знак '?', например 2? + ?5 = 69:");
         String[] arr = scanner.nextLine().split(" ");
-        System.out.println("Вариант решения:\n" + recoverExp(arr));
+        System.out.println("Варианты решения:\n" + recoverExp(arr));
     }
-    static String recoverExp(String[] arr){
+
+    static String recoverExp(String[] arr) {
+        ArrayList<String> result = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 int q = Integer.parseInt(arr[0].replace("?", Integer.toString(i)));
                 int w = Integer.parseInt(arr[2].replace("?", Integer.toString(j)));
                 int e = Integer.parseInt(arr[4]);
-                if((q + w) == e) return q + " + " + w + " = " + e;
+                if ((q + w) == e) {
+                    String s = q + " + " + w + " = " + e;
+                    result.add(s);
+                }
             }
         }
-        return "Решения нет";
+        if (result.isEmpty()) return "Решения нет";
+        else return String.join(" \n", result);
     }
 }
+
 
 
 
